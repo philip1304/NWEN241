da43246af368a87609e785a3a102641f61b46c14
Philip Chang <132544684+philip1304@users.noreply.github.com>
2025-04-22 20:43:26 +1200

    Calculator
    Basic calculator application in C.

---
+++ calculator.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+	// C Practice Project
+	// Optional: loop for multiple calculations until user exits, better input handling (e.g., invalid operator)
+
+int main(void) {
+	double num1, num2, result;
+	char operator;
+	
+	// User input
+	printf("Enter calculation (e.g., 5 + 3): ");
+	scanf("%lf %c %lf", &num1, &operator, &num2);
+
+	// Calculation
+	switch (operator) {
+	case '+':
+		result = num1 + num2;
+		break;
+	case '-':
+		result = num1 - num2;
+		break;
+	case '*':
+		result = num1 * num2;
+		break;
+	case '/':
+		if (num2 == 0) {
+			printf("Error: Cannot divide by 0!\n");
+			return 1;
+		}
+		result = num1 / num2;
+		break;
+	default:
+		printf("Invalid operator.\n");
+		return 1;
+	}
+
+	// Print result
+	printf("Result: %.2lf\n", result);
+}
