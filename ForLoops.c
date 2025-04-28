e11e904e0f46d91f708156e502e5c5944e65dc8e
Philip Chang <132544684+philip1304@users.noreply.github.com>
2025-04-28 17:13:50 +1200

    For Loop Revision

---
+++ ForLoops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	
+	// for loop = Repeat some code a limited # of times.
+	//			  for(Initialization; Condition; Update)
+
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
